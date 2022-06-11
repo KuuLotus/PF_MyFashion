17c6cf4a13f391ff1e164723e21d65d501234709
KuuLotus <kuulotusy@gmail.com>
2022-06-11 17:12:44 +0000

    マイページレイアウト調整

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,7 +8,7 @@ class Member < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :relationships, foreign_key: :following_id, dependent: :destroy
-  has_many :following, through: :relationships, source: :follower
+  has_many :followings, through: :relationships, source: :follower
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :following
   has_one_attached :profile_image
